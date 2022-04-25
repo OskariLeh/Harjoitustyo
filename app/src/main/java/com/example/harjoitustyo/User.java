1805418b47299888040f4d090474a66e26e6ae02
Riikka Kuopio <riikka.kuopio@gmail.com>
2022-04-25 15:11:59 +0300

    User class created.

---
+++ app/src/main/java/com/example/harjoitustyo/User.java
@@ -0,0 +1,42 @@
+package com.example.harjoitustyo;
+
+import java.util.ArrayList;
+
+public class User {
+    private String name;
+    private String passcode;
+    private String email;
+    private ArrayList<String> favouriteLakes = new ArrayList<>();
+
+    public User(){
+    }
+
+    public void addLakeToFavorite (String lake) {
+        favouriteLakes.add(lake);
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPasscode() {
+        return passcode;
+    }
+
+    public void setPasscode(String passcode) {
+        this.passcode = passcode;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
