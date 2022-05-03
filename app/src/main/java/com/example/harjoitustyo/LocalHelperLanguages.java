444b2682b897138efa1f46f0ca78ed5f72b567cc
Riikka Kuopio <riikka.kuopio@gmail.com>
2022-05-03 19:17:29 +0300

    User class created.

---
+++ app/src/main/java/com/example/harjoitustyo/LocalHelperLanguages.java
@@ -0,0 +1,21 @@
+package com.example.harjoitustyo;
+
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.os.Build;
+import android.preference.PreferenceManager;
+
+
+import java.util.Locale;
+
+//https://www.geeksforgeeks.org/how-to-change-the-whole-app-language-in-android-programmatically/
+//Riikka
+//This class is for changing the languages
+public class LocalHelperLanguages {
+    private static final String SELECTED_LANGUAGE = Locale.Helper.Selected.Language;
+
+}
